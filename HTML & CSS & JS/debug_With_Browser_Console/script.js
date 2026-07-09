bcc6f4425e288ea65d4191d26572924fa3917e5a
mycode100 <mvsr26032005@gmail.com>
2026-07-09 16:13:19 +0530

    Add HTML CSS JS folder

---
+++ HTML & CSS & JS/debug_With_Browser_Console/script.js
@@ -0,0 +1,23 @@
+const card = document.getElementById("card");
+const deleteBtn = document.getElementById("deleteBtn");
+
+card.addEventListener("click", function () {
+
+    console.log("Opening Invoice Details...");
+
+});
+
+deleteBtn.addEventListener("click", function () {
+
+    console.log("Deleting Invoice...");
+
+});
+
+
+// deleteBtn.addEventListener("click", function (event) {
+
+//     event.stopPropagation();
+
+//     console.log("Deleting Invoice...");
+
+// });
---
+++ HTML & CSS & JS/debug_With_Browser_Console/script1.js
@@ -0,0 +1,13 @@
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function () {
+
+    const user = document.getElementById("userName");
+
+    console.log(user.value);
+
+//     const user = document.getElementById("username");
+
+// console.log(user.value);
+
+});
